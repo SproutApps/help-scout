76d564db2e88181b004d3d978d596fdc4f121164
Dan Cameron <dan@sproutapps.co>
2016-10-14 10:28:32 -0700

    Deploying version 4.0

--- views/section/conversation-tags.php
+++
@@ -1,3 +0,0 @@
-<?php foreach ( $converation_tags as $key => $tag ) : ?>
-	<span class="label label-default label-<?php esc_attr_e( $tag ) ?>"><?php echo $tag ?></span> 
-<?php endforeach ?>
--- views/section/tag-selection.php
+++
@@ -1,9 +0,0 @@
-<div class="form-group">
-	<label for="hs_thread_type" class="hidden"><?php _e( 'Select Topic', 'help-scout-desk' ) ?></label>
-	<select name="hs_thread_type" class="hsd_select select_input">
-		<option><?php _e( 'Select Topic', 'help-scout-desk' ) ?></option>
-		<?php foreach ( $tags as $key => $tag ) : ?>
-			<option value="<?php esc_attr_e( $tag->id ) ?>"><?php esc_attr_e( $tag->tag ) ?></option>
-		<?php endforeach ?>
-	</select>
-</div>
