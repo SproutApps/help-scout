6dfc28c8c230718c7437135c9c4db2ceb506a087
Dan Cameron <dan@sproutapps.co>
2020-01-16 08:32:42 -0800

    Deploying version 6.3.6

--- HelpScout_Desk.php
+++ HelpScout_Desk.php
@@ -34,7 +34,7 @@ abstract class HelpScout_Desk {
 	/**
 	 * Current version. Should match help-scout-desk.php plugin version.
 	 */
-	const HSD_VERSION = '6.3.5';
+	const HSD_VERSION = '6.3.6';
 	/**
 	 * DB Version
 	 */
